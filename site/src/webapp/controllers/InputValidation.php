a459a19eb7e2b40c58765f32b6d21936a4b46ebb
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-15 14:40:39 +0100

    das

--- site/src/webapp/controllers/InputValidation.php
+++ site/src/webapp/controllers/InputValidation.php
@@ -7,7 +7,7 @@ use Slim\Middleware\Flash;
     class InputValidation {
         function validUserName($username)
         {
-            if(username == null){
+            if($username == null){
                 $this->app->flash("Username field can't be empty");
                 return FALSE; 
             }
