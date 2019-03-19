57e3df5776cae8f2d3b4ae0da968c3d8c058a9eb
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-19 17:56:37 +0100

    a

--- site/src/webapp/controllers/Controller.php
+++ site/src/webapp/controllers/Controller.php
@@ -34,6 +34,7 @@ class Controller
     
     function hasSessionExpired(){
         if(Auth::isSessionExpired()){
+            $this->app->flash('info', 'Your session has expired. Please log in again.');
             $this->app->redirect('/expired');
         }
         if (!Auth::check()) {
