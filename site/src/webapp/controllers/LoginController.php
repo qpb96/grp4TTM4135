bd9303cb973264963d63505fe04adaf1ad432052
qpb96 <quitpb@stud.ntnu.no>
2019-03-20 09:44:40 +0100

    captcha

--- site/src/webapp/controllers/LoginController.php
+++ site/src/webapp/controllers/LoginController.php
@@ -40,6 +40,20 @@ class LoginController extends Controller
         
         $username = $input_handler->get('username');
         $password = $input_handler->get('password');
+        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
+  {
+        $secret = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success)
+        {
+            $succMsg = 'Your contact request have submitted successfully.';
+        }
+        else
+        {
+            $errMsg = 'Robot verification failed, please try again.';
+        }
+   }
 
         if($this->validation->isValidUserName($username) == TRUE && $this->validation->isValidPassword($password)){
             if ( Auth::checkCredentials($username, $password) ) {
