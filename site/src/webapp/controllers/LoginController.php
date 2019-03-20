0c6d553631726b4e3154e7b2970ce678801a5501
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-20 15:08:44 +0100

    j

--- site/src/webapp/controllers/LoginController.php
+++ site/src/webapp/controllers/LoginController.php
@@ -41,6 +41,37 @@ class LoginController extends Controller
         $recaptcha = new ReCaptcha('6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe');
         $resp = $recaptcha->verify($request->get('g-recaptcha'), $request->getIp());
         
+        if(isset($_POST['submit']) && !empty($_POST['submit'])){ 
+
+            // check do we have recaptcha param added to form and submited
+            if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){ 
+                
+                //your site secret recaptcha key
+                $secret = 'YOUR-SITE-SECRET-RECAPTCHA-KEY';
+         
+                 
+                //get verify response data
+                $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+                
+                $responseData = json_decode($verifyResponse);
+         
+                 // check was the response successfully checked by Google
+                if($responseData->success){
+                      // if recaptcha check was success
+                    $succMsg = 'Your contact request have submitted successfully.';
+                    exit($succMsg); 
+                }else{
+                    // if not show the error
+                    $errMsg = 'Robot verification failed, please try again.';
+                    echo $errMsg;
+         
+                }
+                 
+            }else{
+                // if recaptcha is not checked
+                $errMsg = 'Please click on the reCAPTCHA box.';
+            } 
+        } 
   
 
         if (!$resp->isSuccess()) {
