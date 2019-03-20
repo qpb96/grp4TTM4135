246f454affc914468b45902dab2a51d0d224cfa1
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-20 17:43:13 +0100

    sad

--- site/src/webapp/controllers/AuthController.php
+++ site/src/webapp/controllers/AuthController.php
@@ -5,14 +5,14 @@ namespace ttm4135\webapp\controllers;
 use PHPGangsta_GoogleAuthenticator;
 use ttm4135\webapp\Auth;
 
-require_once("../src/webapp/Tools.php");
 
 class AuthController extends Controller {
 
 
     function index() {
+        $this->app->request();
         $secretFactory = new GoogleAuthenticator\SecretFactory();
-        $secret = $secretFactory->create("TTM4135gr18", $username);
+        $secret = $secretFactory->create("TTM4135gr18", "$asd");
         $auth_key = $secret->getSecretKey();
         $qrImageGenerator = new GoogleAuthenticator\QrImageGenerator\GoogleQrImageGenerator();
         $auth_url = $qrImageGenerator->generateUri($secret);
