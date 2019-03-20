3cbacb7f976996292500824bcad15412cc9ee38e
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-20 15:18:35 +0100

    update recapatcha

--- site/src/webapp/controllers/LoginController.php
+++ site/src/webapp/controllers/LoginController.php
@@ -41,72 +41,65 @@ class LoginController extends Controller
         $recaptcha = new ReCaptcha('6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe');
         $resp = $recaptcha->verify($request->get('g-recaptcha'), $request->getIp());
         
-        if(isset($_POST['submit']) && !empty($_POST['submit'])){ 
 
-            // check do we have recaptcha param added to form and submited
-            if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){ 
-                
-                //your site secret recaptcha key
-                $secret = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
-         
-                 
-                //get verify response data
-                $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
-                
-                $responseData = json_decode($verifyResponse);
-         
-                 // check was the response successfully checked by Google
-                if($responseData->success){
-                      // if recaptcha check was success
-                    $succMsg = 'Your contact request have submitted successfully.';
-                    exit($succMsg); 
+            $input_handler = new InputSanitizer($request);
+            $this->validation = new InputValidation();
+            
+            $username = $input_handler->get('username');
+            $password = $input_handler->get('password');
+
+            if(isset($_POST['submit']) && !empty($_POST['submit'])){ 
+
+                // check do we have recaptcha param added to form and submited
+                if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){ 
+                    
+                    //your site secret recaptcha key
+                    $secret = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
+             
+                     
+                    //get verify response data
+                    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+                    
+                    $responseData = json_decode($verifyResponse);
+             
+                     // check was the response successfully checked by Google
+                    if($responseData->success){
+                          // if recaptcha check was success
+#                        $succMsg = 'Your contact request have submitted successfully.';
+                        if($this->validation->isValidUserName($username) == TRUE && $this->validation->isValidPassword($password)){
+                            if ( Auth::checkCredentials($username, $password) ) {
+                                $user = User::findByUser($username);
+                                //Set session when user logs in
+                                UserController::setCookieUsername($username);	
+                                Auth::login($user->getId());
+                                $this->app->flash('info', "You are now successfully logged in as " . $user->getUsername() . ".");
+                                $this->app->redirect('/');
+                            } else {
+                                $this->app->flashNow('error', 'Incorrect username/password combination.');
+                                $this->render('login.twig', []);
+                            }
+                        }
+                        else{
+                            print($username);
+                            print($password);
+                            $this->app->flash("error", "Invalid input in username or password");
+                            $this->render('login.twig', []);            
+                        }
+                    }else{
+                        // if not show the error
+                        $errMsg = 'Robot verification failed, please try again.';
+                        echo $errMsg;
+                    }
+                     
                 }else{
-                    // if not show the error
-                    $errMsg = 'Robot verification failed, please try again.';
-                    echo $errMsg;
-         
-                }
-                 
-            }else{
-                // if recaptcha is not checked
-                $errMsg = 'Please click on the reCAPTCHA box.';
-                $this->app->flash("error", "Invalid input in username or password");
-                $this->render('login.twig', []);  
+                    // if recaptcha is not checked
+                    $errMsg = 'Please click on the reCAPTCHA box.';
+                } 
             } 
-        } 
-  
+      
+    
 
-#        if (!$resp->isSuccess()) {
-#            $this->app->flash("info", "Prove us that you are not a bot");
-#            $this->app->redirect("/login");
-#
- #       }else{
- #           $input_handler = new InputSanitizer($request);
- #           $this->validation = new InputValidation();
- #           
- #           $username = $input_handler->get('username');
- #           $password = $input_handler->get('password');
- #   
- #           if($this->validation->isValidUserName($username) == TRUE && $this->validation->isValidPassword($password)){
- #               if ( Auth::checkCredentials($username, $password) ) {
- #                   $user = User::findByUser($username);
- #                   //Set session when user logs in
- #                   UserController::setCookieUsername($username);	
- #                   Auth::login($user->getId());
- #                   $this->app->flash('info', "You are now successfully logged in as " . $user->getUsername() . ".");
- #                   $this->app->redirect('/');
- #               } else {
- #                   $this->app->flashNow('error', 'Incorrect username/password combination.');
- #                   $this->render('login.twig', []);
- #               }
- #           }
- #           else{
- #               print($username);
- #               print($password);
- #               $this->app->flash("error", "Invalid input in username or password");
- #               $this->render('login.twig', []);            
- #           }
- #       }
+        }
           
 
     }
