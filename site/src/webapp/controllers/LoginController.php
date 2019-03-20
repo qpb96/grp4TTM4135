dd6afeafe1c3f150fa6c8f5829ffb875cda684f9
qpb96 <quitpb@stud.ntnu.no>
2019-03-20 10:57:04 +0100

    Update LoginController.php

--- site/src/webapp/controllers/LoginController.php
+++ site/src/webapp/controllers/LoginController.php
@@ -41,7 +41,7 @@ class LoginController extends Controller
         $username = $input_handler->get('username');
         $password = $input_handler->get('password');
 
-        if($this->validation->isValidUserName($username) == TRUE && $this->validation->isValidPassword($password)){
+        if(true){#$this->validation->isValidUserName($username) == TRUE && $this->validation->isValidPassword($password)){
             if ( Auth::checkCredentials($username, $password) ) {
                 $user = User::findByUser($username);
                 //Set session when user logs in
