161b20d5349def837a3e24c7dcf470bed90710e6
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-20 17:00:36 +0100

    kl

--- site/src/webapp/controllers/LoginController.php
+++ site/src/webapp/controllers/LoginController.php
@@ -49,26 +49,7 @@ class LoginController extends Controller
             
             $username = $input_handler->get('username');
             $password = $input_handler->get('password');
-
-require_once 'PHPGangsta/GoogleAuthenticator.php';
-
-$ga = new PHPGangsta_GoogleAuthenticator();
-$secret = $ga->createSecret();
-echo "Secret is: ".$secret."\n\n";
-
-$qrCodeUrl = $ga->getQRCodeGoogleUrl('Blog', $secret);
-echo "Google Charts URL for the QR-Code: ".$qrCodeUrl."\n\n";
-
-$oneCode = $ga->getCode($secret);
-echo "Checking Code '$oneCode' and Secret '$secret':\n";
-
-$checkResult = $ga->verifyCode($secret, $oneCode, 2);    // 2 = 2*30sec clock tolerance
-if ($checkResult) {
-    echo 'OK';
-} else {
-    echo 'FAILED';
-}
-
+            
 
             if(isset($_POST['submit']) && !empty($_POST['submit'])){ 
 
