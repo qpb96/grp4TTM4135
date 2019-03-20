0417810b910a5c3b82b87b875460b5d3490ea532
qpb96 <quitpb@stud.ntnu.no>
2019-03-20 10:43:22 +0100

    ds

--- site/src/webapp/controllers/LoginController.php
+++ site/src/webapp/controllers/LoginController.php
@@ -34,6 +34,20 @@ class LoginController extends Controller
 
     function login()
     {
+        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
+        {
+              $secret = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
+              $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+              $responseData = json_decode($verifyResponse);
+              if($responseData->success)
+              {
+                  $succMsg = 'Your contact request have submitted successfully.';
+              }
+              else
+              {
+                  $errMsg = 'Robot verification failed, please try again.';
+              }
+         }
 
 
         $request = $this->app->request;
